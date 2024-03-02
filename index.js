8e31a9d2c6de8a7c1a6897ba3d078e96879b0cda
freezy_jungle <131402436+sknyazev23@users.noreply.github.com>
2024-03-02 18:41:54 +0400

    Update index.js
    a count of visitors was added.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 document.getElementById('demoTime').innerHTML = new Date();
+
+let count = localStorage.getItem('visitorCount');
+if (!count) {
+  count = 0;
+}
+count++;
+localStorage.setItem('visitorCount', count);
+document.getElementById('visitorCount').innerHTML = `Number of visitors: ${count}`;
