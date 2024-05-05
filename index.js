ab14ea155241011b974c923c9dce6f390fa6fbe2
Sergey <sknyazev23@gmail.com>
2024-05-05 19:51:45 +0300

    adding_query_as_eventHandler

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const currentDate = new Date();
 document.getElementById('demoTime').innerHTML = `Visit time:  ${currentDate}`;
 
-let count = localStorage.getItem('visitorCount');
+
+let count = document.querySelector('visitorCount');
 if (!count) {
   count = 0;
 }
 count++;
-localStorage.setItem('visitorCount', count);
+// localStorage.setItem('visitorCount', count);
 document.getElementById('visitorCount').innerHTML = `Number of visitors:  ${count}`;
